0f157cd544d9db9534709af7442a40428f73b2e5
Dev.Jin <nara04040@naver.com>
2022-06-23 22:26:26 +0900

    jeongyookgak closebtn active, refresh fix top,scroll header fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+$(document).ready(function (){
+    let popup = $('.popup')
+    let popup_close = $('.popup-first-exit')
+    let header = $('.header')
+    // 닫기 눌렀을때 창 닫힘
+    popup_close.click(function(){
+        popup.addClass('popup-close')
+    })
+    // Scroll
+    $(window).scroll(function () {
+        // 위치
+        let temp = $(window).scrollTop();
+        // 숨기기
+        if(temp > 60) {
+            header.addClass('header-fix')
+        }else{
+            header.removeClass('header-fix')
+        }
+    })
+    // 새로고침했을때 페이지 상단으로
+    $(function(){
+        setTimeout(function(){
+            scrollTo(0,0);
+        }, 100)
+    })
+})
+
 window.onload = function () {
     new Swiper('.sw-event', {
         pagination: {
