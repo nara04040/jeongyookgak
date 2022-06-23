f0b2622883ee37eebf097b8dc307a0c729dc6f7e
Dev.Jin <nara04040@naver.com>
2022-06-23 21:34:06 +0900

    정육각 제출준비완료

--- js/script.js
+++ js/script.js
@@ -1,12 +1,5 @@
 window.onload = function () {
     new Swiper('.sw-event', {
-        // loop: true,
-        // autoplay: {
-        //     delay: 2500,
-        //     disableOnInteraction: false,
-        // },
-        // speed: 1000,
-        // 페이지 버튼, 화살표
         pagination: {
             el: ".swiper-pagination",
             type: "fraction",
