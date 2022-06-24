c47ee59dfeee4bdc92f2f969d359705af0585171
Dev.Jin <nara04040@naver.com>
2022-06-25 00:04:45 +0900

    add hamber

--- js/script.js
+++ js/script.js
@@ -2,15 +2,21 @@ $(document).ready(function (){
     let popup = $('.popup')
     let popup_close = $('.popup-first-exit')
     let header = $('.header')
+    let icon_x = $('.more-div-close')
     // 닫기 눌렀을때 창 닫힘
     popup_close.click(function(){
         popup.addClass('popup-close')
+        icon_x.addClass('more-div-none')
     })
     // Scroll
     $(window).scroll(function () {
         // 위치
         let temp = $(window).scrollTop();
-        // 숨기기
+        // if(popup_close) {
+        //     header.addClass('header-fix')
+        // } else {
+
+        //숨기기
         if(temp > 60) {
             header.addClass('header-fix')
         }else{
