7cf4afc4cfa8a890efddd1af5c94a20d39f11846
Dev.Jin <nara04040@naver.com>
2022-06-28 00:07:49 +0900

    fixed position and 배경사라지기 못함

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function () {
     let popup = $('.popup')
     let popup_close = $('.popup-first-exit')
     let header = $('.header')
@@ -6,18 +6,23 @@ $(document).ready(function (){
     let open_menu = $('.open-menu')
     let more_wrap = $('.more-wrap')
     // 닫기 눌렀을때 창 닫힘
-    popup_close.click(function(){
+    popup_close.click(function () {
         popup.addClass('popup-close')
     })
     // 햄버거 눌렀을때 나타나기
-    open_menu.click(function(){
+    open_menu.click(function () {
         more_wrap.addClass('open-menu')
     })
     // 닫기버튼 눌렀을때 닫히기 
-    icon_x.click(function(){
+    icon_x.click(function () {
         more_wrap.removeClass('open-menu')
     })
-    // Scroll
+    // 배경누르면 더보기 사라지기
+    more_wrap.on('click', function(){
+        $('.more-wrap').addClass('.more-wrap-hide')
+        more_wrap.fadeOut();
+    })
+
     $(window).scroll(function () {
         // 위치
         let temp = $(window).scrollTop();
@@ -26,16 +31,16 @@ $(document).ready(function (){
         // } else {
 
         //숨기기
-        if(temp > 60) {
+        if (temp > 60) {
             header.addClass('header-fix')
-        }else{
+        } else {
             header.removeClass('header-fix')
         }
     })
     // 새로고침했을때 페이지 상단으로
-    $(function(){
-        setTimeout(function(){
-            scrollTo(0,0);
+    $(function () {
+        setTimeout(function () {
+            scrollTo(0, 0);
         }, 100)
     })
 })
